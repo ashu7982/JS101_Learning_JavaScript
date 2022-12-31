4d977026371bb14a9012b50ddc9f245a16f7de74
ashu7982 <[ashutoshdubey7982@gmail.com]>
2022-12-31 13:01:13 +0530

    Day 1 Programme

---
+++ variables1/problem4.js
@@ -0,0 +1,10 @@
+// Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+// Print the report card of the student (You can make it look nice by using some keyboard symbols )
+console.log("Name = Ashutosh");
+console.log("School = Arpan Public School");
+console.log("Grade = 9th");
+console.log("Section = A");
+console.log("Roll No. = 22");
+console.log("Marks obtained in History = 58");
+console.log("Marks obtained in Maths = 76");
+console.log("Marks obtained in English = 92");
